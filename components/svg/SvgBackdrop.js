b6c535418a0dbb6de558bad39e308a376fd944e0
Tom <tom@bearjam.dev>
2020-02-04 13:07:35 +0000

    layout changes

---
+++ components/svg/SvgBackdrop.js
@@ -0,0 +1,421 @@
+import React from "react";
+
+function SvgBackdrop(props) {
+  return (
+    <svg
+      viewBox="0 0 630.651 108.541"
+      {...props}
+    >
+      <defs>
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__c"
+          id="desktopscenelight_svg__t"
+          x1={-1.33}
+          y1={226.504}
+          x2={6.246}
+          y2={259.766}
+          gradientUnits="userSpaceOnUse"
+        />
+        <linearGradient id="desktopscenelight_svg__g">
+          <stop offset={0} stopColor="#a0ec24" />
+          <stop offset={1} stopColor="#ff8038" stopOpacity={0.8} />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__e">
+          <stop offset={0} stopColor="#3dcf24" />
+          <stop offset={1} stopColor="#3d4224" />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__h">
+          <stop offset={0} stopColor="#27db24" />
+          <stop offset={1} stopColor="#ffd638" stopOpacity={0.8} />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__i">
+          <stop offset={0} stopColor="#ef5b0f" />
+          <stop offset={1} stopColor="#b53624" />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__f">
+          <stop offset={0} stopColor="#3db824" />
+          <stop offset={1} stopColor="#3d5924" />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__c">
+          <stop offset={0} stopColor="#3d7624" />
+          <stop offset={1} stopColor="#3d4224" />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__b">
+          <stop offset={0} stopColor="#44ca35" />
+          <stop offset={1} stopColor="#44ca35" stopOpacity={0} />
+        </linearGradient>
+        <linearGradient id="desktopscenelight_svg__a">
+          <stop offset={0} stopColor="#e2d739" />
+          <stop offset={1} stopColor="#e2d739" stopOpacity={0} />
+        </linearGradient>
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__a"
+          id="desktopscenelight_svg__j"
+          x1={101.562}
+          y1={240.25}
+          x2={101.562}
+          y2={297.98}
+          gradientUnits="userSpaceOnUse"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__a"
+          id="desktopscenelight_svg__m"
+          gradientUnits="userSpaceOnUse"
+          x1={107.442}
+          y1={190.051}
+          x2={106.676}
+          y2={298.028}
+          gradientTransform="matrix(-1 0 0 1 420.31 0)"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__b"
+          id="desktopscenelight_svg__n"
+          gradientUnits="userSpaceOnUse"
+          x1={100.493}
+          y1={208.477}
+          x2={101.169}
+          y2={302.021}
+          gradientTransform="matrix(-1 0 0 1 420.31 0)"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__a"
+          id="desktopscenelight_svg__o"
+          gradientUnits="userSpaceOnUse"
+          x1={101.562}
+          y1={240.25}
+          x2={101.562}
+          y2={297.98}
+          gradientTransform="scale(-1 1)"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__a"
+          id="desktopscenelight_svg__p"
+          gradientUnits="userSpaceOnUse"
+          x1={101.562}
+          y1={240.25}
+          x2={101.562}
+          y2={297.98}
+          gradientTransform="matrix(-1.20253 0 0 1 420.31 0)"
+        />
+        <linearGradient id="desktopscenelight_svg__d">
+          <stop offset={0} stopColor="#3d4324" />
+          <stop offset={1} stopColor="#3d9a24" />
+        </linearGradient>
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__c"
+          id="desktopscenelight_svg__s"
+          x1={39.241}
+          y1={250.031}
+          x2={41.338}
+          y2={264.394}
+          gradientUnits="userSpaceOnUse"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__d"
+          id="desktopscenelight_svg__u"
+          x1={32.526}
+          y1={293.926}
+          x2={24.5}
+          y2={241.387}
+          gradientUnits="userSpaceOnUse"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__e"
+          id="desktopscenelight_svg__r"
+          x1={141.744}
+          y1={221.383}
+          x2={199.476}
+          y2={253.889}
+          gradientUnits="userSpaceOnUse"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__f"
+          id="desktopscenelight_svg__k"
+          gradientUnits="userSpaceOnUse"
+          x1={102.81}
+          y1={253.613}
+          x2={102.81}
+          y2={305.774}
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__g"
+          id="desktopscenelight_svg__q"
+          x1={172.111}
+          y1={267.277}
+          x2={174.052}
+          y2={308.215}
+          gradientUnits="userSpaceOnUse"
+        />
+        <linearGradient
+          xlinkHref="#desktopscenelight_svg__h"
+          id="desktopscenelight_svg__l"
+          x1={6.91}
+          y1={268.539}
+          x2={8.971}
+          y2={300.611}
+          gradientUnits="userSpaceOnUse"
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__v"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          gradientUnits="userSpaceOnUse"
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__x"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__y"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__z"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__A"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__B"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__C"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+        <radialGradient
+          xlinkHref="#desktopscenelight_svg__i"
+          id="desktopscenelight_svg__w"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(.42745 3.44403 -2.49784 .31002 324.135 -1001.674)"
+          cx={301.158}
+          cy={61.597}
+          fx={301.158}
+          fy={61.597}
+          r={5.471}
+        />
+      </defs>
+      <g fillRule="evenodd">
+        <path
+          d="M0 244.08s34.21-6.414 56.127 0c21.916 6.415 51.85 9.622 59.333 7.484 7.484-2.138 20.313 1.604 28.866.535 8.552-1.07 46.605 3.107 46.605 3.107H0z"
+          fill="url(#desktopscenelight_svg__j)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M-210.155 255.206H0v42.172h-210.155zm210.155 0h210.155v42.172H0zm210.155 0H420.31v42.172H210.155z"
+          fill="url(#desktopscenelight_svg__k)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M-45.829 261.281c-9.578.19-83.384 4.725-57.092 8.127 26.293 3.402 65.731.756 60.848 4.913-4.883 4.158-50.33 3.024-27.043 9.45 23.287 6.425 68.89 5.914 68.89 5.914l.138-25.055zm91.186 1.512c1.512 0 83.155 3.213 56.697 6.615-26.459 3.402-66.146.756-61.233 4.913 4.914 4.158 50.65 3.024 27.215 9.45-23.435 6.425-68.262 5.914-68.262 5.914l.138-25.055z"
+          fill="url(#desktopscenelight_svg__l)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M420.31 201.05s-41.695-26.994-79.647-.267c-37.952 26.727-50.246 4.276-73.766 5.345-23.52 1.07-22.985 17.106-36.349 19.778-13.363 2.673-20.355.406-20.355.406l-.038 28.894H420.31zM1.055 194.568s-21.583-8.795-61.804 8.483c-42.65 18.321-48.36 13.11-74.522 4.59-14.397-4.69-37.348 17.86-50.712 20.533-13.363 2.673-24.092-2.268-24.092-2.268l-.08 29.3H0zm0 0s26.378 2.865 73.245 23.32c35.26 15.39 55.592-12.829 79.112-11.76 23.52 1.07 26.058 8.42 38.353 13.898 12.448 5.548 18.441 6.264 18.441 6.264l-.052 28.916H0z"
+          opacity={0.823}
+          fill="url(#desktopscenelight_svg__m)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M420.042 217.086s-11.492-16.303-36.616-6.147c-25.123 10.156-28.865 28.865-48.643 20.847-19.778-8.018-35.814-1.603-48.643 4.811-12.829 6.415-33.141 5.345-43.297 2.138-10.157-3.207-28.76.802-32.502 1.337.038 4.975-.186 15.134-.186 15.134H420.31zM.905 202.714s-12.665-1.931-37.788 8.225c-25.124 10.156-28.865 28.865-48.643 20.847-19.778-8.018-35.814-1.603-48.643 4.811-12.83 6.415-33.142 5.345-43.298 2.138-10.156-3.207-29.132.802-32.874 1.337.038 4.975.186 15.134.186 15.134H0zm0 0s10.855-1.931 35.978 8.225c25.124 10.156 28.865 28.865 48.643 20.847 19.778-8.018 35.814-1.603 48.643 4.811 12.83 6.415 33.142 5.345 43.298 2.138 10.156-3.207 32.215 1.357 32.874 1.337.033 4.916-.186 15.134-.186 15.134H0z"
+          opacity={0.608}
+          fill="url(#desktopscenelight_svg__n)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M0 244.08s-34.21-6.414-56.127 0c-21.916 6.415-51.85 9.622-59.333 7.484-7.484-2.138-20.313 1.604-28.866.535-8.552-1.07-46.504 3.207-46.504 3.207l190.83-.1z"
+          fill="url(#desktopscenelight_svg__o)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M420.31 244.08s-41.14-6.414-67.494 0c-26.355 6.415-62.352 9.622-71.351 7.484-9-2.138-24.426 1.604-34.711.535-10.285-1.07-55.823 3.107-55.823 3.107h229.378z"
+          fill="url(#desktopscenelight_svg__p)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M210.155 255.206s33.075 2.455 20.602 6.215c-12.473 3.76 64.634 1.88 38.554 9.4-26.08 7.519-1.512 11.654 30.616 10.902 32.128-.752 78.24 11.28 41.955 13.911-36.286 2.632-131.727 1.745-131.727 1.745m0-42.173s-63.41-.035-66.816 7.19c-2.068 5.073 19.74 5.396 22.39 5.776 2.649.38-11.415 9.053-36.393 11.334-24.978 2.282-91.586 18.743-25.735 17.983 65.852-.76 106.554-.11 106.554-.11z"
+          fill="url(#desktopscenelight_svg__q)"
+          transform="translate(210.341 -188.973)"
+        />
+      </g>
+      <g fillRule="evenodd">
+        <path
+          d="M169.32 246.793s-31.865 10.597-29.743-7.916c1.452-12.664 21.848-5.729 20.512-7.065-1.336-1.336-6.133-35.447 19.884-33.485 23.657 1.785 8.59 34.3 8.59 34.3s13.137-10.485 19.667.15c6.063 9.875-8.108 18.105-19.713 16.79-7.628-.863-19.198-2.774-19.198-2.774z"
+          fill="url(#desktopscenelight_svg__r)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M378.966 73.087c-.189-.567 1.134-35.34 1.134-35.34s.85 35.53.756 35.718c-.094.19-1.89-.378-1.89-.378z"
+          fill="#3d3124"
+        />
+        <path
+          d="M380.384 52.393c.472-.095 17.481-4.82 17.481-4.82l-17.67 6.237-17.954-3.685z"
+          fill="#3d3124"
+        />
+        <path
+          d="M40.523 243s-8.552 24.321-6.147 25.925c2.405 1.604 11.76 1.07 11.493-.267C45.6 267.32 40.523 243 40.523 243z"
+          fill="url(#desktopscenelight_svg__s)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M2.914 209.134s-23.3 58.956-21.535 61.074c1.765 2.119 38.128 2.119 39.187.353 1.059-1.765-17.652-61.427-17.652-61.427z"
+          fill="url(#desktopscenelight_svg__t)"
+          transform="translate(210.341 -188.973)"
+        />
+        <path
+          d="M26.223 234.658S8.583 279.292 9.92 280.896c1.336 1.603 28.865 1.603 29.666.267.802-1.336-13.363-46.505-13.363-46.505z"
+          fill="url(#desktopscenelight_svg__u)"
+          transform="translate(210.341 -188.973)"
+        />
+      </g>
+      <g fill="url(#desktopscenelight_svg__v)">
+        <path d="M296.046 69.294s-.709 2.84-.548 3.667c.142.731.29 1.304.351 1.644.161.883.087 1.531.306 2.571.219 1.04-.72 1.394-.63 1.78.138.588.63 1.012 1.834.793 1.205-.219.712-1.204.712-1.642 0-.438 1.423-3.504 2.19-3.832.766-.328 1.235.13 1.751.712.849.958 1.205.702 1.205 2.135 0 1.368-.548 1.67.711 1.67 1.15 0 2.134-.092 1.396-1.041-.766-.985.246-3.804.465-4.954.22-1.15-.592-3.588-.975-4.464l-2.44-6.973-4.238.387z" />
+        <path
+          d="M299.311 64.965s.05.498-.299.846c-.348.348-1.194 1.792-1.244 1.99-.05.2.548 1.095.946 1.543.398.448-1.045 1.642-1.792 1.294-.746-.348-1.244-2.438-1.194-2.936.05-.498-.846-2.687-.746-4.33.1-1.642 1.791-4.926 1.84-5.324.05-.398.05-3.932 1.444-5.076 1.393-1.145 3.971-.289 3.981-.299.024.019 1.244.597 1.294 3.733.05 3.135-.05 2.488.547 3.533.598 1.045 1.593 3.931 1.045 6.519-.547 2.588-.647 3.782-1.692 3.384-1.045-.398-1.443-1.443-.846-2.04.598-.598.846-1.294.697-1.543-.15-.249-.995-.846-1.045-1.095-.05-.249-.15-.846-.15-.846"
+          stroke="#000006"
+          strokeWidth={0.18}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M302.142 52.739s.317-.7.951-.212c.635.488-.144 1.165-.144 1.165M298.523 52.895s-.432-.73-.969-.291c-.536.439-.405 1.317.034 1.512"
+          stroke="#000006"
+          strokeWidth={0.176}
+          strokeLinejoin="round"
+        />
+        <path
+          d="M298.41 63.295c-.018-.104-.311-3.605 1.655-3.623 1.967-.017 2.157 2.433 2.122 3.278M299.462 55.24s-.173-1.588 1.07-1.657c1.241-.069 1.224 1.311 1.224 1.69"
+          stroke="#000006"
+          strokeWidth={0.176}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <path
+        d="M300.117 53.652s-.224-.362-.517-.155c-.294.207.01.698.01.698M301.187 53.739s.276-.276.483-.104c.207.173.034.483-.087.57M300.773 55.36l-.225-.38-.276-.051s-.051-.535.449-.552c.5-.017.5.397.5.535 0 .151.014.126-.241.12z"
+        stroke="#00000b"
+        strokeWidth={0.176}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M300.065 55.55s.363-.207.708-.19c.345.017.534.19.534.19s-.552-.104-.586.138l-.017.103s.086-.327-.639-.241z"
+        stroke="#00000b"
+        strokeWidth={0.176}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M296.046 69.294l.476-.986" fill="#ed6524" fillRule="evenodd" />
+      <g>
+        <g
+          fill="url(#desktopscenelight_svg__w)"
+          transform="rotate(2.91 157.196 638.764) scale(.8514)"
+        >
+          <path
+            d="M296.046 69.294s-.709 2.84-.548 3.667c.142.731.29 1.304.351 1.644.161.883.087 1.531.306 2.571.219 1.04-.72 1.394-.63 1.78.138.588.63 1.012 1.834.793 1.205-.219.712-1.204.712-1.642 0-.438 1.423-3.504 2.19-3.832.766-.328 1.235.13 1.751.712.849.958 1.205.702 1.205 2.135 0 1.368-.548 1.67.711 1.67 1.15 0 2.134-.092 1.396-1.041-.766-.985.246-3.804.465-4.954.22-1.15-.592-3.588-.975-4.464l-2.44-6.973-4.238.387z"
+            fill="url(#desktopscenelight_svg__x)"
+          />
+          <path
+            d="M299.311 64.965s.05.498-.299.846c-.348.348-1.194 1.792-1.244 1.99-.05.2.548 1.095.946 1.543.398.448-1.045 1.642-1.792 1.294-.746-.348-1.244-2.438-1.194-2.936.05-.498-.846-2.687-.746-4.33.1-1.642 1.791-4.926 1.84-5.324.05-.398.05-3.932 1.444-5.076 1.393-1.145 3.971-.289 3.981-.299.024.019 1.244.597 1.294 3.733.05 3.135-.05 2.488.547 3.533.598 1.045 1.593 3.931 1.045 6.519-.547 2.588-.647 3.782-1.692 3.384-1.045-.398-1.443-1.443-.846-2.04.598-.598.846-1.294.697-1.543-.15-.249-.995-.846-1.045-1.095-.05-.249-.15-.846-.15-.846"
+            fill="url(#desktopscenelight_svg__y)"
+            stroke="#000006"
+            strokeWidth={0.18}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M302.142 52.739s.317-.7.951-.212c.635.488-.144 1.165-.144 1.165"
+            fill="url(#desktopscenelight_svg__z)"
+            stroke="#000006"
+            strokeWidth={0.176}
+            strokeLinejoin="round"
+          />
+          <path
+            d="M298.523 52.895s-.432-.73-.969-.291c-.536.439-.405 1.317.034 1.512"
+            fill="url(#desktopscenelight_svg__A)"
+            stroke="#000006"
+            strokeWidth={0.176}
+            strokeLinejoin="round"
+          />
+          <path
+            d="M298.41 63.295c-.018-.104-.311-3.605 1.655-3.623 1.967-.017 2.157 2.433 2.122 3.278"
+            fill="url(#desktopscenelight_svg__B)"
+            stroke="#000006"
+            strokeWidth={0.176}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M299.462 55.24s-.173-1.588 1.07-1.657c1.241-.069 1.224 1.311 1.224 1.69"
+            fill="url(#desktopscenelight_svg__C)"
+            stroke="#000006"
+            strokeWidth={0.176}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+        <path
+          d="M285.481 51.433s-.175-.318-.433-.155c-.258.164-.022.594-.022.594M286.387 51.552s.246-.223.415-.067c.169.156.008.412-.098.48M285.965 52.913l-.174-.332-.233-.056s-.02-.457.405-.45c.426.007.408.359.403.476-.006.129.006.108-.211.092zM285.355 53.044s.317-.16.61-.13c.292.029.446.183.446.183s-.465-.111-.504.092l-.02.088s.088-.275-.532-.233z"
+          stroke="#00000b"
+          strokeWidth={0.1498464}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path d="M281.343 64.557l.448-.818" fill="#ed6524" fillRule="evenodd" />
+      </g>
+    </svg>
+  );
+}
+
+export default SvgBackdrop;
+
