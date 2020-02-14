c90a6b72481ed8e95739ae5f7541a78554021f69
Tom <tom@bearjam.dev>
2020-02-14 09:59:01 +0000

    a start

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useTheme } from 'emotion-theming';
-import React, { createElement } from 'react';
+import React, { createElement, useState } from 'react';
 import tw from 'tailwind.macro';
 import { SvgSun } from '../components';
 import OptInForm from '../components/OptInForm';
@@ -9,6 +9,7 @@ import SvgBannerPath from '../components/svg/SvgBannerPath';
 import { BackdropLayout } from '../layouts';
 import { useMediaQuery } from 'beautiful-react-hooks';
 import { css } from '@emotion/core';
+import { useAnimation } from 'framer-motion';
 
 const border = tw`border-2 border-red-500 border-solid`
 
@@ -34,56 +35,52 @@ const cloudStyles = tw`
 const ran = x => Math.random() * x;
 const ranIn = (x1, x2) => ran(x2 - x1) + x1;
 const items = n => [...Array(n).keys()]
+const Box = ({ as, children, ...props }) => createElement(as, props, children);
 
 const IndexPage = () => {
   const { colors } = useTheme();
-  const clouds = ({ n, y, duration, dx, rx, ry, offset = 0 }) => {
-    let last = {
-      x: (-0.2) * (n*dx) + offset,
-      y
-    }
-    return (
-      <>
-        {items(n).map(i => {
-          let next = {
-            x: last.x + dx + ran(rx),
-            y: last.y + ranIn(-(ry/2), ry/2)
-          };
-          const el = (
-            <SvgBabyCloud
-              css={[ cloudStyles, css`top: 1rem;` ]}
-              key={i}
-              initial={{
-                x: `${last.x}rem`,
-                y: `${last.y}rem`,
-                opacity: i === 0 ? 0 : i % 2 == 0 ? 0.2 : 0.7,
-                scale: i === 0 ? 0.7 : i % 2 == 0 ? 0.7 : 1.2
-              }}
-              animate={{
-                x: `${next.x}rem`,
-                y: `${next.y}rem`,
-                opacity: i === (n-1) ? 0 : i % 2 == 0 ? 0.7 : 0.2,
-                scale: i === (n-1) ? 0.7 : i % 2 == 0 ? 1.2 : 0.7
-              }}
-              transition={{
-                ease: [ 0.55, 0.45, 0.55, 0.45],
-                duration,
-                loop: Infinity
-              }}
-            />
-          );
-          last = next;
-          return el;
-        })}
-      </>
-    )
-  }
+  const controls = useAnimation();
+  const [d, setD] = useState(2);
+  const [w, setW] = useState(30);
+
+  const animations = [
+    i => ({
+      x: `${(w + (i*10)) / 2}vw`,
+      transition: {
+        duration: d / 2,
+        ease: "linear"
+      }
+    }),
+    i => ({
+      x: `-${w + (i*10)}vw`,
+      transition: {
+        duration: 0,
+      }
+    }),
+    i => ({
+      x: `${w + (i*10)}vw`,
+      transition: {
+        duration: d,
+        loop: Infinity,
+        ease: "linear"
+      }
+    }),
+  ].reduce((acc, v) => acc.then(async () => controls.start(v)), Promise.resolve());
+
+  controls.start(animations);
   return (
     <div css={mainColumnStyles}>
-      {clouds({ n: 3, y: 0, duration: 8, dx: 24, rx: 2, ry: 0.5, offset: 0 })}
-      {clouds({ n: 4, y: 6, duration: 10, dx: 18, rx: 2, ry: 0.5, offset: -12 })}
-      {clouds({ n: 5, y: 12, duration: 12, dx: 12, rx: 2, ry: 0.5, offset: 0 })}
-      <SvgSun css={tw`h-24 xl:h-32 relative z-20`} fill={colors.yellow[400]} stroke={colors.gray[700]} animate={{ opacity: [0.9, 0.95, 0.9]}} transition={{ loop: Infinity, duration: 0.3, ease: [ 0.55, 0.45, 0.55, 0.45 ] }}/>
+      {items(2).map(i =>
+        <Box
+          as={i % 2 === 0 ? SvgBabaCloud : SvgBabyCloud}
+          css={cloudStyles}
+          initial={i => ({ x: i * 10 })}
+          animate={controls}
+          custom={i}
+          key={i}
+        />
+      )}
+      <SvgSun css={tw`h-24 xl:h-32 relative z-20`} fill={colors.yellow[400]} stroke={colors.gray[700]} animate={{ opacity: [0.9, 0.95, 0.9] }} transition={{ loop: Infinity, duration: 0.3, ease: [0.55, 0.45, 0.55, 0.45] }} />
       <SvgBannerPath css={tw`h-12 xl:h-20 my-8 xl:my-10 relative z-40`} fill={colors.blue[100]} stroke={colors.gray[700]} />
       <OptInForm css={tw`h-10 mx-2 xl:mb-32 relative z-40`} />
     </div>
